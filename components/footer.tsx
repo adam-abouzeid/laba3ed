fad1476d902085c782779b536ddef0ab12f43b74
adam-abouzeid <adamabouzeid04@gmail.com>
2024-09-25 01:47:33 +0300

    ADDED: mail

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="bottom-0 left-0 w-full text-center text-black py-4 bg-gray-100">
       Created by{" "}
       <a
-        href="mailto:someone@example.com"
+        href="mailto:adamabouzeid04@gmail.com"
         className="text-black hover:underline"
       >
         Adam Abou Zeid
