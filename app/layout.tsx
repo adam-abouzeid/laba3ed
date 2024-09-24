206ba395ec2f0e1c329842fc71369267f5116d9a
adam-abouzeid <adamabouzeid04@gmail.com>
2024-09-24 23:33:36 +0300

    Small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { Toaster } from "sonner";
+import Footer from "@/components/footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Navbar />
         <Toaster />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className="bottom-0 left-0 w-full text-center text-black py-4 bg-gray-100">
+      Made by{" "}
+      <a
+        href="mailto:someone@example.com"
+        className="text-black hover:underline"
+      >
+        Adam Abou Zeid
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
