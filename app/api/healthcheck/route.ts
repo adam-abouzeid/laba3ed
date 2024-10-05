62db38fa3775ad5d8501fb0115753ae36a5b91b4
adam-abouzeid <adamabouzeid04@gmail.com>
2024-10-05 12:25:14 +0300

    upd

--- app/api/healthcheck/route.ts
+++ app/api/healthcheck/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   return new NextResponse(JSON.stringify({ message: "Healthcheck passed." }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
