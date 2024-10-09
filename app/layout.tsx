f9929260d61b344be1bd23cf135bce5fa3f94f43
adel121 <adel.hajhassan@datadoghq.com>
2024-10-09 20:15:00 +0200

    set_text_direction_to_rtl_for_arabic_locale

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default async function RootLayout({
   return (
     <html
       lang={locale}
+      dir={locale === "ar" ? "rtl" : "ltr"}
       className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
     >
       <body className="flex flex-col">
